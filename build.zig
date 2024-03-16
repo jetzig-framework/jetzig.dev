bfc9fb2cab1830588b3b9486f022b9e883e22a33
Bob Farrell <git@bob.frl>
2024-03-16 17:39:35 +0000

    Initial commit

---
+++ build.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const jetzig = @import("jetzig");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "jetzig.dev.zig",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    try jetzig.jetzigInit(b, exe, .{});
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| run_cmd.addArgs(args);
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub const jetzig = @import("jetzig");
+pub const routes = @import("routes").routes;
+
+pub const jetzig_options = struct {
+    pub const middleware: []const type = &.{
+        jetzig.middleware.HtmxMiddleware,
+    };
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    const app = try jetzig.init(allocator);
+    defer app.deinit();
+
+    try app.start(comptime jetzig.route(routes));
+}
