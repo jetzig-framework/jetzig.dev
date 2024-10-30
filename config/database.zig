7780df54cd85efb296d49028b114cf08c7c99a2a
Bob Farrell <git@bob.frl>
2024-10-30 21:42:49 +0000

    WIP

--- config/database.zig
+++ config/database.zig
@@ -1,8 +1,28 @@
 pub const database = .{
-    .adapter = .postgresql,
-    .hostname = "localhost",
-    .port = 5432,
-    .username = "postgres",
-    .password = "password",
-    .database = "jetzig_website",
+    .testing = .{
+        .adapter = .postgresql,
+        .hostname = "localhost",
+        .port = 5432,
+        .username = "postgres",
+        .password = "password",
+        .database = "jetzig_website_testing",
+    },
+
+    .development = .{
+        .adapter = .postgresql,
+        .hostname = "localhost",
+        .port = 5432,
+        .username = "postgres",
+        .password = "password",
+        .database = "jetzig_website_development",
+    },
+
+    .production = .{
+        .adapter = .postgresql,
+        .database = "jetzig_website",
+        .hostname = "localhost",
+        .port = 5432,
+        .username = "postgres",
+        .password = "password",
+    },
 };
