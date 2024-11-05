16ce8a0e2028669dad47f5e5ff0f1e6a996267da
Bob Farrell <git@bob.frl>
2024-11-05 21:35:43 +0000

    WIP

--- src/app/database/Schema.zig
+++ src/app/database/Schema.zig
@@ -1,14 +1,14 @@
-const jetzig = @import("jetzig");
+const jetquery = @import("jetzig").jetquery;
 
-pub const Blogs = jetzig.jetquery.Model(
+pub const Blogs = jetquery.Model(
     @This(),
     "blogs",
     struct {
         id: i32,
         title: []const u8,
         content: []const u8,
-        created_at: jetzig.DateTime,
-        updated_at: jetzig.DateTime,
+        created_at: jetquery.DateTime,
+        updated_at: jetquery.DateTime,
     },
     .{},
 );
